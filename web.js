25ed886bb4b14d7f39e5e9712c2093b822b677f1
Soe Lin Myat <soelinmyat@gmail.com>
2013-07-19 23:17:47 +0800

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+  var buf = fs.readFileSync('index.html');
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
